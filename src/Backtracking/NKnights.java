1346d3cb3fad1f2a9adac403b376638d32059d16
SUMIT <rais74184@gmail.com>
2024-12-12 14:11:25 +0530

    new

---
+++ src/Backtracking/NKnights.java
@@ -0,0 +1,82 @@
+package Backtracking;
+
+public class NKnights {
+    static void knight(boolean [][]board,int row,int col,int target){
+        if(target==0){
+            display(board);
+            System.out.println();
+            return;
+        }
+
+        if(row== board.length-1 && col== board.length){
+            return;
+        }
+
+        if(col== board.length){
+            knight(board,row+1,0,target);
+            return;
+        }
+
+        if(isSafe(board,row,col)){
+            board[row][col]=true;
+            knight(board,row,col+1,target-1);
+            board[row][col]=false;  //backtrack from here
+        }
+        knight(board,row,col+1,target);  //if we are unsafe to place then move ahead in the board
+
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        if(isValid(board, row-2, col-1)){
+        if(board[row-2][col-1]){
+            return false;
+             }
+        }
+        if(isValid(board, row-1, col-2)){
+            if(board[row-1][col-2]){
+                return false;
+            }
+        }
+        if(isValid(board, row-2, col+1)){
+            if(board[row-2][col+1]){
+                return false;
+            }
+        }
+        if(isValid(board, row-1, col+2)){
+            if(board[row-1][col+2]){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    static boolean isValid(boolean[][] board,int row,int col){
+//        This function is to check our knight is not moving out of the matrix
+        if(row>=0 && row < board.length && col>=0 && col < board.length){
+            return true;
+        }
+        return false;
+    }
+
+    private static void display(boolean[][] board) {
+        for(boolean[] row:board){
+            for(boolean element:row){
+                if(element){
+                    System.out.print("K");
+                }else{
+                    System.out.print("X");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n=4;
+        boolean [][]board=new boolean[n][n];
+        knight(board,0,0,4);
+    }
+}
---
+++ src/Backtracking/Sudoku.java
@@ -0,0 +1,103 @@
+package Backtracking;
+
+public class Sudoku {
+
+    static boolean isSafe(int [][] board,int row,int col,int num){
+//        check the row
+        for(int i=0;i<board.length;i++){
+            if(board[row][i]==num){
+                return false;
+            }
+        }
+//        check for the col
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int sqrt=(int)(Math.sqrt(board.length));
+        int rowStart=row-row % sqrt;
+        int colStart=col-col % sqrt;
+
+
+        for(int r=rowStart;r<rowStart+sqrt;r++){
+            for(int c=colStart;c<colStart+sqrt;c++){
+            if(board[r][c]==num){
+                return false;
+            }
+            }
+
+        }
+        return true;
+    }
+
+    static boolean solver(int [][]board){
+        int n= board.length;
+        int row=-1;
+        int col=-1;
+
+        boolean ItemsPlace=true;
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==0){
+                    row=i;
+                    col=j;
+                    ItemsPlace=false;
+                    break;
+                }
+            }
+            if(!ItemsPlace){
+                break;
+            }
+        }
+        if(ItemsPlace){
+            return true;
+            //sudoku is solved
+        }
+        for (int number=1;number<=9;number++){
+            if(isSafe(board,row,col,number)){
+                board[row][col]=number;
+                if(solver(board)){
+//                    display(board);
+                    return true;// board is solved
+                }
+            }
+            board[row][col]=0;
+        }
+        return false;
+//        after multiple attempts if the board is not solved then return false
+    }
+
+    private static void display(int[][] board) {
+       for(int[] row:board){
+           for(int num:row){
+               System.out.print(num+ " ");
+           }
+           System.out.println();
+       }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[][] board = new int[][]{
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
+//        System.out.println(solver(board));
+        if(solver(board)){
+            display(board);
+        }else{
+            System.out.println("cannot solve the board");
+        }
+    }
+
+}
