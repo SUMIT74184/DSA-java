b161a6fb2f9de16ca4658299838f5a91daff551c
sumit rai <RAIS74184@gmail.com>
2025-02-15 16:02:27 +0530

    wordproblem

--- src/Graph/Problems/Flood.java
+++ src/Graph/Problems/Flood.java
@@ -1,4 +1,60 @@
 package Graph.Problems;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Flood {
+    int rows;
+    int cols;
+    //we need to color the 1's in the matrix starting from the 1x1
+    //also need to check the out-off bound condition which are 4 directionally
+    public void dfs(int row,int col,int color,int currColor,boolean [][]visited,int [][]image){
+        if(row<0 || row>=rows || col<0 || col>=cols || image[row][col]!=currColor || visited[row][col]){
+            return;
+        }
+        image[row][col]=color;
+        visited[row][col]=true;
+        //neighbors
+        int [][]adjList={{row-1,col},{row,col+1},{row+1,col},{row,col-1}};
+        for(int []neighbour:adjList){
+            dfs(neighbour[0],neighbour[1],color,currColor,visited,image);
+        }
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        //visited array is important
+        rows= image.length;
+        cols= image[0].length;
+        boolean [][]visited=new boolean[rows][cols];
+//        dfs(sr,sc,color,image[sr][sc],visited,image);
+//        return image;
+
+//        breadth first search graph
+        int curColor=image[sr][sc];
+        Queue<int[]>queue=new LinkedList<>();
+        queue.offer(new int[]{sr,sc});
+        image[sr][sc]=color; //fill and visited at the same time
+        while (!queue.isEmpty()){
+            int []node=queue.poll();
+            int row=node[0];
+            int col=node[1];
+            int [][]adjList={{row-1,col},{row,col+1},{row+1,col},{row,col-1}};
+            for(int []neighbour:adjList){
+                int r=neighbour[0];
+                int c=neighbour[1];
+                if(r<0 || r>=rows || c<0 || c>=cols || image[r][c]!=curColor || image[r][c]==color){
+                    continue;
+                }
+                queue.offer(new int[]{r,c});
+                image[r][c]=color;
+
+            }
+        }
+//        dfs(sr,sc,color,image[sr][sc],visited,image);
+        return image;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
---
+++ src/Graph/Problems/wordLadder.java
@@ -0,0 +1,57 @@
+package Graph.Problems;
+
+import java.util.*;
+
+public class wordLadder {
+    public List<String> getNeighbours(String word, HashSet<String> set){
+        List<String>neighbours=new ArrayList<>();
+        for(int i=0;i<word.length();i++){
+            for(char ch='a';ch<='z';ch++){
+                if(ch==word.charAt(i)){
+                    continue;
+                }
+                String newWord =word.substring(0,i)+ch+word.substring(i+1,word.length());
+                if(set.contains(newWord)){
+                    neighbours.add(newWord);
+                }
+            }
+        }
+        return neighbours;
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String>set=new HashSet<>(wordList);
+        if(!set.contains(endWord)){
+            return 0;
+        }
+        Queue<String> queue=new LinkedList<>();
+        queue.offer(beginWord);
+        if(set.contains(beginWord)){
+            set.remove(beginWord);
+        }
+        int level=0;
+        while(!queue.isEmpty()){
+            int currLevelSize=queue.size();
+            //level wise traversal
+            for(int i=0;i<currLevelSize;i++){
+                String node=queue.poll();
+                if(node.equals(endWord)){
+                    return level+1;
+                }
+                //adjList
+                List<String>neighbours=getNeighbours(node,set);
+                for(String word:neighbours){
+                    if(set.contains(word)){
+                        queue.offer(word);
+                        set.remove(word);
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
